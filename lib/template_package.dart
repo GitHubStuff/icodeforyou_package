c4ab542d6a6b051228584a4a8fddc4d7ae3834b5
Steven <github@icodeforyou.com>
2024-09-05 14:56:31 -0400

    Initial commit

---
+++ lib/template_package.dart
@@ -0,0 +1,3 @@
+/// The name of the library is the same as the package name
+// TODO(any): Rename this AND the file to match your package name
+library template_package;
---
+++ test/a_flutter_package_template_test.dart
@@ -0,0 +1,7 @@
+import 'package:flutter_test/flutter_test.dart';
+// ignore: unused_import
+import 'package:template_package/template_package.dart';
+
+void main() {
+  test('adds one to input values', () {});
+}
