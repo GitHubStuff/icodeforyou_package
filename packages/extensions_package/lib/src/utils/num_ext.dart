0189bd82f0eace2ffda20b0fab520a043ef22f5f
Steven Smith <github@icodeforyou.com>
2024-10-16 15:37:00 -0400

    Small tweek to double

--- packages/extensions_package/lib/src/utils/num_ext.dart
+++ packages/extensions_package/lib/src/utils/num_ext.dart
@@ -1,5 +1,12 @@
+import 'dart:math';
+
 extension DoubleExt on double {
   double get pct => this / 100.0;
+  double roundTo(int? places) {
+    final mod = pow(10.0, places ?? 2);
+    // ignore: unnecessary_parenthesis
+    return ((this * mod).round().toDouble() / mod);
+  }
 }
 
 extension IntExt on int {
